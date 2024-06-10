1f0ffb251e4a9b942f5f70b5b4338ddade61c96a
ivanapanowicz <ivanapanowicz@gmail.com>
2024-06-10 13:39:25 -0300

    Login page

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,6 +1,36 @@
 const Login = () => {
   return (
-        <h1>Login</h1>
+    <div className="max-w-md mx-auto flex items-center flex-col justify-center h-screen">
+      <header className="flex flex-col w-full">
+        <div className="flex">
+          <img src="logo.png" alt="HandyHeroes Logo" className="w-8 rounded-full" />
+          <h1 className="text-2xl font-bold text-blue-900 ml-2">HandyHeroes</h1>
+        </div>
+        <p className="mt-12 mb-2 text-gray-600">Bienvenido</p>
+        <h2 className="font-bold text-4xl text-gray-600">Continúe con su cuenta</h2>
+      </header>
+      <main className="w-full">
+        <form className="mt-12 w-full">
+          <div className="mb-4 w-full">
+            <input type="email" id="email" placeholder="Correo Electrónico" className="appearance-none border border-gray-300 rounded w-full py-4 px-3 leading-tight focus:outline-none focus:border-gray-500  text-gray-700" />
+          </div>
+          <div className="mb-4 relative">
+            <input type="password" id="password" placeholder="Contraseña" className="appearance-none border border-gray-300 rounded w-full py-4 px-3 leading-tight focus:outline-none focus:border-gray-500  text-gray-700 pr-10" />
+            <div className="absolute inset-y-0 right-0 flex items-center px-2">
+              <i className="fas fa-eye text-gray-400"></i>
+            </div>
+          </div>
+          <div>
+            <button
+              className="bg-blue-500 w-full text-white px-6 py-3 rounded-md mr-2 transition duration-300 hover:bg-blue-600 font-bold">CONTINUAR</button>
+          </div>
+          <div>
+            <a className="block mt-12 mb-6 text-gray-500 hover:text-gray-700 cursor-pointer text-center w-full">¿Eres nuevo? COMENZAR - ES GRATIS</a>
+          </div>
+        </form>
+
+      </main>
+    </div>
   )
 }
 
