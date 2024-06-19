69528ff8c4f7719df1a705c784dfda52e898b870
abranico <abranico011@gmail.com>
2024-06-19 15:27:14 -0300

    add icons

--- frontend/src/components/ui/icons/index.js
+++ frontend/src/components/ui/icons/index.js
@@ -1,1 +1,5 @@
 export {default as SearchIcon} from './SearchIcon'
+export {default as PhoneIcon} from './PhoneIcon'
+export {default as MapIcon} from './MapIcon'
+export {default as EmailIcon} from './EmailIcon'
+export {default as StarIcon} from './StarIcon'
--- frontend/src/pages/Services/Services.jsx
+++ frontend/src/pages/Services/Services.jsx
@@ -4,8 +4,8 @@ import { SearchIcon } from "../../components/ui/icons";
 const Services = () => {
   return (
     <>
-      <header className="bg-sky-800 px-6 py-11  text-white ">
-        <h1 className="mx-14 text-3xl font-bold">
+      <header className="px-6 py-11  text-white bg-gradient-to-r from-cyan-600 to-blue-600 ">
+        <h1 className="mx-14 text-4xl font-bold">
           Busca el profesional y servicio deseado.
         </h1>
         <div className="mx-14 flex mt-9 py-5 gap-7">
