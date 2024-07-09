eced7d680b5f64c84a3459eaaaf9f19c53eaa7e1
ivanapanowicz <ivanapanowicz@gmail.com>
2024-07-09 18:54:53 -0300

    reviews context

---
+++ frontend/src/context/reviews/reviews.context.jsx
@@ -0,0 +1,17 @@
+import { createContext } from "react";
+import useReviews from "../../hooks/useReviews";
+export const ReviewsContext = createContext();
+
+export const ReviewsContextProvider = ({ children }) => {
+  const { reviews, loading, removeReview } = useReviews();
+  console.log(reviews);
+  const deleteReview = (id) => {
+    removeReview(id);
+  };
+
+  return (
+    <ReviewsContext.Provider value={{ reviews, deleteReview }}>
+      {!loading && children}
+    </ReviewsContext.Provider>
+  );
+};
